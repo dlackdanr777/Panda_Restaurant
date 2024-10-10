8f19339e5b47f9a37e7947d2970bd9121bf6de64
임창묵 <ckdanr1234@gmail.com>
2024-10-11 02:48:34 +0900

    [Add] 미니게임 UI 배치중

--- Assets/Scripts/Muks/RecyclableScrollView/RecyclableVerticalScrollView.cs
+++ Assets/Scripts/Muks/RecyclableScrollView/RecyclableVerticalScrollView.cs
@@ -1,54 +1,42 @@
 using System.Collections.Generic;
-using UnityEngine.UI;
 using UnityEngine;
 
 namespace Muks.RecyclableScrollView
 {
-    public abstract class RecyclableVerticalScrollView<T> : MonoBehaviour
+    public abstract class RecyclableVerticalScrollView<T> : RecyclableScrollView<T>
     {
-        [Header("Components")]
-        [SerializeField] protected ScrollRect _scrollRect; // ScrollRect 컴포넌트
-        [SerializeField] protected RectTransform _contentRect; // Content 트랜스폼
-        [SerializeField] protected RecyclableScrollSlot<T> _slotPrefab; // 슬롯 프리팹
-
         [Space]
-        [Header("Option")]
-        [SerializeField] protected int _bufferRowCount = 5; // 여유 있게 로드할 슬롯 세로 줄수
-        [SerializeField] protected float _left;
-        [SerializeField] protected float _right;
-        [SerializeField] protected float _top;
-        [SerializeField] protected float _bottom;
-        [SerializeField] protected float _spacing;
+        [Header("VerticalScrollView Option")]
+        [SerializeField] protected int _itemsPerRow = 1;
+        [SerializeField] protected float _topOffset;
+        [SerializeField] protected float _bottomOffset;
+        [SerializeField] protected float _horizontalOffset;
 
-        protected LinkedList<RecyclableScrollSlot<T>> _slotList = new LinkedList<RecyclableScrollSlot<T>>();
-        protected List<T> _dataList = new List<T>(); // 데이터를 저장하는 리스트
-        protected float _itemHeight;
-        protected float _itemWidth;
-        protected int _poolSize;
-        protected int _tmpfirstVisibleIndex;
-        protected int _contentVisibleSlotCount;
-        protected int _itemsPerRow; // 한 줄에 들어가는 아이템 개수
-
-        public virtual void Init(List<T> dataList)
+      
+        public override void Init(List<T> dataList)
         {
-            _dataList = dataList; // 데이터를 설정
+            _dataList = dataList;
+
+            RectTransform scrollRectTransform = _scrollRect.GetComponent<RectTransform>();
             // 슬롯 크기
             _itemHeight = _slotPrefab.Height;
             _itemWidth = _slotPrefab.Width;
 
-            // 가로에 들어가는 아이템 개수 계산
-            _itemsPerRow = Mathf.FloorToInt(_scrollRect.GetComponent<RectTransform>().rect.width / _itemWidth);
-
             // 전체 높이 계산
             int totalRows = Mathf.CeilToInt((float)_dataList.Count / _itemsPerRow);
-            float contentHeight = _itemHeight * totalRows + ((totalRows - 1) * _spacing) + _top + _bottom;
+            float contentHeight = _itemHeight * totalRows + ((totalRows - 1) * _spacing) + _topOffset + _bottomOffset;
 
-            _contentVisibleSlotCount = (int)(_scrollRect.GetComponent<RectTransform>().rect.height / _itemHeight) * _itemsPerRow;
+            //Anchor값 고정(계산 오류 방지)
+            _contentRect.anchorMax = new Vector2(1f, 1f);
+            _contentRect.anchorMin = new Vector2(0f, 1f);
+
+            //contentRect의 높이 계산
+            _contentVisibleSlotCount = (int)(scrollRectTransform.rect.height / _itemHeight) * _itemsPerRow;
             _contentRect.sizeDelta = new Vector2(_contentRect.sizeDelta.x, contentHeight);
 
             // 슬롯 생성 및 리스트에 추가
-            _poolSize = _contentVisibleSlotCount + (_bufferRowCount * 2 * _itemsPerRow);
-            int index = -_bufferRowCount * _itemsPerRow;
+            _poolSize = _contentVisibleSlotCount + (_bufferCount * 2 * _itemsPerRow);
+            int index = -_bufferCount * _itemsPerRow;
             for (int i = 0; i < _poolSize; i++)
             {
                 RecyclableScrollSlot<T> item = Instantiate(_slotPrefab, _contentRect);
@@ -60,10 +48,12 @@ namespace Muks.RecyclableScrollView
         }
 
 
-        protected void UpdateData(List<T> dataList)
+        public override void UpdateData(List<T> dataList)
         {
             _dataList = dataList;
-            int index = _tmpfirstVisibleIndex - _bufferRowCount * _itemsPerRow;
+
+            //예비 슬롯들을 고려해 index 세팅 및 Update
+            int index = _tmpfirstVisibleIndex - _bufferCount * _itemsPerRow;
             foreach (RecyclableScrollSlot<T> item in _slotList)
             {
                 UpdateSlot(item, index);
@@ -72,11 +62,11 @@ namespace Muks.RecyclableScrollView
         }
 
 
-        /// <summary>슬롯의 위치를 변경하는 함수</summary>
-        private void OnScroll(Vector2 scrollPosition)
+        protected override void OnScroll(Vector2 scrollPosition)
         {
             float contentY = _contentRect.anchoredPosition.y;
 
+            //현재 인덱스 위치 계산 
             int firstVisibleRowIndex = Mathf.Max(0, Mathf.FloorToInt(contentY / (_itemHeight + _spacing)));
             int firstVisibleIndex = firstVisibleRowIndex * _itemsPerRow;
 
@@ -95,7 +85,7 @@ namespace Muks.RecyclableScrollView
                         _slotList.RemoveFirst();
                         _slotList.AddLast(item);
 
-                        int newIndex = lastVisibleIndex + (_bufferRowCount * _itemsPerRow) + i;
+                        int newIndex = lastVisibleIndex + (_bufferCount * _itemsPerRow) + i;
                         UpdateSlot(item, newIndex);
                     }
                 }
@@ -109,7 +99,7 @@ namespace Muks.RecyclableScrollView
                         _slotList.RemoveLast();
                         _slotList.AddFirst(item);
 
-                        int newIndex = _tmpfirstVisibleIndex - (_bufferRowCount * _itemsPerRow) - i;
+                        int newIndex = _tmpfirstVisibleIndex - (_bufferCount * _itemsPerRow) - i;
                         UpdateSlot(item, newIndex);
                     }
                 }
@@ -118,22 +108,25 @@ namespace Muks.RecyclableScrollView
             }
         }
 
-        /// <summary>슬롯의 데이터를 업데이트하고 위치를 갱신하는 함수</summary>
-        private void UpdateSlot(RecyclableScrollSlot<T> item, int index)
+
+        protected override void UpdateSlot(RecyclableScrollSlot<T> item, int index)
         {
+            //현재 Index의 행과 열을 계산
             int row = 0 <= index ? index / _itemsPerRow : (index - 1) / _itemsPerRow;
             int column = Mathf.Abs(index) % _itemsPerRow;
-            Vector2 pivot = item.RectTransform.pivot;
 
+            // X축 및 Y축 위치 계산 (가로를 기준으로 중앙 정렬 및 피벗 보정)
+            Vector2 pivot = item.RectTransform.pivot;
             float totalWidth = (_itemsPerRow * (_itemWidth + _spacing)) - _spacing;
             float contentWidth = _contentRect.rect.width;
             float offsetX = (contentWidth - totalWidth) / 2f;
             float adjustedY = -(row * (_itemHeight + _spacing)) - _itemHeight * (1 - pivot.y);
             float adjustedX = column * (_itemWidth + _spacing) + _itemWidth * pivot.x;
-            adjustedX += offsetX + _left - _right;
-            adjustedY -= _top;
+            adjustedX += offsetX + _horizontalOffset;
+            adjustedY -= _topOffset;
             item.RectTransform.localPosition = new Vector3(adjustedX, adjustedY, 0);
 
+            //Index가 입력된 DataList의 크기를 넘어가거나 0미만이면 슬롯을 끄고 Update를 진행하지 않는다.
             if (index < 0 || index >= _dataList.Count)
             {
                 item.gameObject.SetActive(false);
