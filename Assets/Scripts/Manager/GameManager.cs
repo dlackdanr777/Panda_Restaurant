7c7d4c3faa40579399d78e600a52142392cf1f06
dlackdanr777 <64966089+dlackdanr777@users.noreply.github.com>
2026-06-18 20:09:38 +0900

    [Fix] 코드 최적화

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -439,8 +439,11 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        if (_instance != null)
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
             return;
+        }
 
         _instance = this;
         DontDestroyOnLoad(gameObject);
